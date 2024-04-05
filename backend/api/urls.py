2790fedfbbdfbdcbe45381c0da9b7afc648f5df8
DarkyWarky <292mhatre@gmail.com>
2024-04-05 18:09:03 +0530

    Added Views for filtering tasks in Backend

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns =[
     path('tasks/',views.TasksCreate.as_view(),name='tasks-list'),
-    path('notes/delete/<int:pk>/',views.TasksDelete.as_view(),name ="delete-task")
+    path('tasks/delete/<int:pk>/',views.TasksDelete.as_view(),name ="delete-task"),
+    path('tasks/priority/', views.TasksPriorityFilter.as_view(), name="tasks-priority-filter"),
+    path('tasks/completed/', views.TasksCompletedFilter.as_view(), name="tasks-completed-filter"),
+    path('tasks/remaining/', views.TasksRemainingFilter.as_view(), name="tasks-remaining-filter"),
 ]
--- backend/api/views.py
+++ backend/api/views.py
@@ -5,6 +5,8 @@ from .serializers import UserSerializer,TasksSerializer
 from rest_framework.permissions import IsAuthenticated,AllowAny
 from .models import Tasks
 
+#--------------------------------------------- Insert & Delete -------------------------------------------#
+
 class TasksCreate(generics.ListCreateAPIView):
     serializer_class = TasksSerializer
     permission_classes = [IsAuthenticated]
@@ -26,6 +28,40 @@ class TasksDelete(generics.DestroyAPIView):
     def get_queryset(self):
         user = self.request.user
         return Tasks.objects.filter(user_id = user)
+
+
+# ------------------------------------------------ Tasks Filters -------------------------------------------------- #
+
+
+class TasksPriorityFilter(generics.ListAPIView):
+    serializer_class = TasksSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        priority = self.request.query_params.get('priority')
+        queryset = Tasks.objects.filter(user_id=user)
+        queryset = queryset.filter(priority=priority)
+        queryset = sorted(queryset, key=lambda x: x.priority)
+        return queryset
+
+class TasksCompletedFilter(generics.ListAPIView):
+    serializer_class = TasksSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        return Tasks.objects.filter(user_id=user, completed=True)
+
+class TasksRemainingFilter(generics.ListAPIView):
+    serializer_class = TasksSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        return Tasks.objects.filter(user_id=user, completed=False)
+
+#-----------------------------------Authorization--------------------------------#
 
 class CreateUserView(generics.CreateAPIView):
     queryset = User.objects.all()
